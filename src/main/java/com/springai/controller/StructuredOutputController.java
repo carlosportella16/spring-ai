b4c5aef5d8945f4f4c94d495539b2142c1175928
Carlos Portella <carlosportella16@hotmail.com>
2026-02-28 12:35:18 -0300

    feat: Configuring the bean for receiving structured response from LLM.

---
+++ src/main/java/com/springai/controller/StructuredOutputController.java
@@ -0,0 +1,35 @@
+package com.springai.controller;
+
+import com.springai.model.CountryCities;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StructuredOutputController {
+
+    private final ChatClient chatClient;
+
+    public StructuredOutputController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @GetMapping("/chat-bean")
+    public ResponseEntity<CountryCities> chatBean(@RequestParam("message") String message) {
+        var countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(CountryCities.class);
+
+        return ResponseEntity.ok(countryCities);
+    }
+
+}
