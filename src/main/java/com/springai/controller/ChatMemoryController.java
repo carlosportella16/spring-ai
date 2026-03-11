c87566a8c9fe2820e16d144836c4ddea211efc85
Carlos Portella <carlosportella16@hotmail.com>
2026-03-10 21:59:57 -0300

    feat: Creating endpoint and config for saving data in memory

---
+++ src/main/java/com/springai/controller/ChatMemoryController.java
@@ -0,0 +1,31 @@
+package com.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+@RestController
+@RequestMapping("/api")
+public class ChatMemoryController {
+
+    private final ChatClient chatClient;
+
+    public ChatMemoryController(@Qualifier("chatMemoryChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/chat-memory")
+    public ResponseEntity<String> chatMemoru(@RequestHeader("username") String username, @RequestParam("message") String message) {
+        return ResponseEntity.ok(
+                chatClient.
+                        prompt()
+                        .user(message)
+                        .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, username))
+                        .call()
+                        .content()
+        );
+    }
+}
